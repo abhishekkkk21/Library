1a7849366ca115f71d88950e04502901e0256d1a
Abhishek Pramanik <abhishekpramanikk21@gmail.com>
2023-09-21 22:01:33 +0530

    Library Management

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2023 Online Library Management System |<a href="https://www.linkedin.com/in/abhishek-pramanik-960434273/" target="_blank"  > Designed by: Abhishek Pramanik</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
